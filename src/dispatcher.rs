7604fb258eb39b04f1630f22ce393ed29dfcef2f
Rasmus Lyxell <rasmuslyxell@gmail.com>
2021-03-23 00:31:50 +0100

    Beans

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -9,9 +9,11 @@ pub struct Dispatcher;
 
 impl Dispatcher{
     pub async fn dispatch(&self, ctx: Context, new_message: &Message, parsed_message: &String){
-        match new_message.channel_id.send_message(&ctx.http, |m: &mut CreateMessage| m.content("Beans")).await{
+        match new_message.channel_id.send_message(&ctx.http, |m: &mut CreateMessage| 
+                m.embed(|e: &mut CreateEmbed| e.colour(Colour::new(16744210)).title("***Beans.***").image("https://cdn.onebauer.media/one/media/5dce/6968/90a4/1691/d1c0/c02c/Heinz+baked+beans.jpg?format=jpg&quality=80&width=960&height=540&ratio=16-9&resize=aspectfill"))
+            ).await{
             Err(why) => println!("Error sending message: {:?}", why),
-            Ok(_) => println!("Dispatched beans....")
+            Ok(_) => println!("Dispatched beans.... with message {:?}", parsed_message)
         }
     }
 }
