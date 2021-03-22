1ce738e7ad23093db07a6993bd4040344dc3c854
Rasmus Lyxell <rasmuslyxell@gmail.com>
2021-03-22 21:19:15 +0100

    Shard creation and super basic event handler set up

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,30 @@ use serenity::{
     prelude::*,
 };
 
+struct CommandHandler;
+
+#[async_trait]
+impl EventHandler for CommandHandler{
+    async fn message(&self, ctx: Context, new_message: Message){
+        
+    }
+
+    async fn ready(&self, _: Context, data_about_bot: Ready){
+        println!("Hello! I am ready to dispatch beans!");
+    }
+}
+
+#[tokio::main]
+async fn main(){
+    let token_result = std::fs::read_to_string(std::path::Path::new("../token"));
+    assert_eq!(token_result.is_ok(), true);
+    let token = token_result.ok().expect("Could not read token from file");
+
+    let mut client = Client::builder(&token)
+        .event_handler(CommandHandler)
+        .await.expect("Error creating client");
+
+    if let Err(why) = client.start().await{
+        println!("Client error encountered: {:?}", why);
+    }
+}
