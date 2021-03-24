790495e8be53771813f9ed0326694861e62f9b3c
Filip Henningsson <c18fhn@cs.umu.se>
2021-03-24 20:10:12 +0100

    Added some attributes to the bean commands

--- src/modules/currency.rs
+++ src/modules/currency.rs
@@ -11,18 +11,33 @@ use crate::backend::currency::*;
 
 #[group]
 #[prefix = "beans"]
+#[description = "A group with commands related to the bean currency"]
+#[summary = "Bean currency commands"]
 #[commands(gimme,showme,give)]
+#[default_command(showme)]
 struct Currency;
 
 #[command]
-pub async fn gimme(ctx: &Context, msg: &Message) -> CommandResult {
+#[owners_only]
+#[description = "Gives a specified amount of beans"]
+#[usage = "[amount]"]
+#[example = "100"]
+#[min_args(0)]
+#[max_args(1)]
+pub async fn gimme(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let userid = msg.author.id.0;
-    add_beans(userid, 5);
-    msg.channel_id.say(&ctx.http, "Here, have `5` beans!").await?;
+    let amount = match args.single::<u32>() {
+        Err(_) => 5,
+        Ok(am) => am
+    };
+    add_beans(userid, amount);
+    msg.channel_id.say(&ctx.http, "Here, have `".to_string() + &amount.to_string() +"` beans!").await?;
     Ok(())
 }
 
 #[command]
+#[description = "Shows your current bean balance"]
+#[max_args(0)]
 pub async fn showme(ctx: &Context, msg: &Message) -> CommandResult {
     let userid = msg.author.id.0;
     let _ = match get_bean_balance(userid) {
@@ -33,12 +48,13 @@ pub async fn showme(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+#[description = "Gives the mentioned user a specified amount of beans"]
+#[usage = "@user amount"]
+#[example = "@Beanlover 50"]
+#[min_args(2)]
+#[max_args(2)]
 pub async fn give(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let userid = msg.author.id.0;
-    if msg.mentions.len() < 1 {
-        msg.channel_id.say(&ctx.http, "Who do you want to give the beans to?").await?;
-        return Ok(());
-    } 
     let recipient = &msg.mentions[0];
     let _ = args.single::<String>(); // First arg is mention
     match args.single::<u32>(){
