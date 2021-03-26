4de80e472698d942e71d2c3ec440f03fa9cdfcdb
Rasmus Lyxell <rasmuslyxell@gmail.com>
2021-03-26 23:56:12 +0100

    Way too verbose but works. Generates until a sentence with bean related words in generated

--- src/backend/markov.rs
+++ src/backend/markov.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 use serenity::Client;
 use serenity::client::Context;
 use serenity::prelude::TypeMapKey;
+use std::io::ErrorKind;
 
 
 pub struct BibleChain;
@@ -14,11 +15,30 @@ impl TypeMapKey for BibleChain{
     type Value = ChainResult;
 }
 
-pub fn generate_sentence(chain: &ChainResult) -> Result<Vec<String>, ()>{
-    match chain {
-        Ok(c) => Ok(c.generate()),
-        Err(_) => Err(())
+pub fn generate_sentence(chain: &ChainResult) -> std::io::Result<Vec<String>>{
+    let mut flag = true;
+    let mut out: std::io::Result<Vec<String>> = Ok(Vec::new());
+    let beans: Vec<&str> = vec!["beans", "bean", "wobean", "wobeans", "beansus", "heinz"];
+    while flag{
+        out = match chain {
+            Ok(c) => {
+                let genned = c.generate();
+                for string in genned.iter(){
+                    for pat in beans.iter(){
+                        if string.to_lowercase().contains(pat){
+                            flag = false;
+                        }
+                    }
+                }
+                Ok(genned)
+            },
+            Err(_) => {
+                flag = false;
+                Err(std::io::Error::new(ErrorKind::InvalidData, "No"))
+            }
+        }
     }
+    return out;
 }
 
 pub async fn init_chain(client: &Context){
