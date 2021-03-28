cf4e34b5b8bdad123041cc547e1a537173cadcb3
Rasmus Lyxell <rasmuslyxell@gmail.com>
2021-03-28 23:13:34 +0200

    Ignorerar nu bottar

--- src/modules/spinthebean.rs
+++ src/modules/spinthebean.rs
@@ -1,5 +1,5 @@
 use serenity::prelude::*;
-use serenity::Result;
+use serenity::{Result};
 use serenity::model::prelude::*;
 use serenity::framework::standard::{CommandResult, macros::*, Args};
 use rand::random;
@@ -21,53 +21,56 @@ pub async fn spinthebean(ctx: &Context, msg: &Message, mut args: Args) -> Comman
         let voice_states: HashMap<UserId, VoiceState>;
         let voice_state: &VoiceState =  match msg.guild(&ctx.cache).await{
             None => {
-                msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel. No guild in cache").await;
+                let _ = msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel. No guild in cache").await;
                 return Ok(())
             }
             Some(guild) => {
                 voice_states = guild.voice_states;
                 match voice_states.get(&msg.author.id){
-                    Some(state) => {
-                        state
-                    },
+                    Some(state) => state,
                     None => {
-                        msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel. You are not in a voice channel").await;
+                        let _ = msg.channel_id.say(&ctx.http, "Join a voice channel or provide a list of items to spin").await;
                         return Ok(())
                     },
                 }
             }
         };
         let channel: GuildChannel = match get_state_guild_channel(ctx, voice_state).await{
-            Ok(chan) => {
-                chan
-            },
+            Ok(chan) => chan,
             Err(_) => {
-                msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel").await;
+                let _ = msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel").await;
                 return Ok(())
             },
         };
         let members: Vec<Member> = match channel.members(&ctx.cache).await{
-            Ok(mem) => mem,
+            Ok(mem) => {
+                let mut filtered = Vec::new();
+                for x in mem.iter(){
+                    if !x.user.bot {
+                        filtered.push(x.clone())
+                    }
+                }
+                filtered
+            },
             Err(_) => {
-                msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel").await;
+                let _ = msg.channel_id.say(&ctx.http, "Could not fetch users in voice channel").await;
                 return Ok(())
             }
         };
-        let rand_index: usize = random::<usize>() % members.len();
-        let result = members.get(rand_index).unwrap();
-        msg.channel_id.say(&ctx.http, format!("The bean landed on {}!", result.display_name())).await;
+        let result = members.get(random::<usize>() % members.len()).unwrap();
+        let _ = msg.channel_id.say(&ctx.http, format!("The bean landed on *{}*!", result.display_name())).await;
     } else{
         let mut valid_args: Vec<String> = Vec::new();
-        let rand_index: usize = random::<usize>() % args.len();
         for _ in 0..args.len(){
             let _ = match args.single_quoted::<String>(){
                 Ok(arg_string) => valid_args.push(arg_string),
                 Err(_) => (),
             };
         }
+        let rand_index: usize = random::<usize>() % valid_args.len();
         for x in 0..valid_args.len(){
             if rand_index == x{
-                msg.channel_id.say(&ctx.http, format!("The bean landed on {}!", valid_args[x])).await;
+                let _ = msg.channel_id.say(&ctx.http, format!("The bean landed on *{}*!", valid_args[x])).await;
                 break;
             }
         }
